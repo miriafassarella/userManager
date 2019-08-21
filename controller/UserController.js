7f429d2d35eace32465271ffacf85fe728239d01
Miriã Fassarella <miriafassarella@gmail.com>
2019-08-21 11:18:28 -0300

    working with date

--- controller/UserController.js
+++ controller/UserController.js
@@ -15,6 +15,10 @@ class UserController{
 
             event.preventDefault();
 
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
             let values = this.getValues();
 
            this.getPhoto().then(//if all goes well do it
@@ -22,6 +26,10 @@ class UserController{
                 values.photo = content; //the content function refers to the result
                     
                 this.addLine(values);
+
+                this.formEl.reset();//cleaning form
+
+                btn.disabled = false;
                 }, 
             (e)=>{
 
@@ -119,7 +127,7 @@ class UserController{
     <td>${dataUser.name}</td>
     <td>${dataUser.email}</td>
     <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-    <td>${dataUser.birth}</td>
+    <td>${Utils.dateFormat(dataUser.register)}</td>
     <td>
         <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
--- models/User.js
+++ models/User.js
@@ -1,16 +1,46 @@
 class User{
 
     constructor(name, gender, birth, country, email, password, photo, admin){
-
-        this.name = name; //saving parameters in variables
-        this.gender = gender;
-        this.birth = birth;
-        this.country = country;
-        this.email = email;
-        this.password = password;
-        this.photo = photo;
-        this.admin = admin;
-
+//the underline is just a convention
+        this._name = name; //saving parameters in variables
+        this._gender = gender;
+        this._birth = birth;
+        this._country = country;
+        this._email = email;
+        this._password = password;
+        this._photo = photo;
+        this._admin = admin;
+        this._register = new Date();
     }
 
+    get register(){
+        return this._register;
+    }
+    get name(){
+        return this._name;
+    }
+    get gender(){
+        return this._gender;
+    }
+    get birth(){
+        return this_birth;
+    }
+    get country(){
+        return this._country;
+    }
+    get email(){
+        return this._email;
+    }
+    get password(){
+        return this._password;
+    }
+    get photo(){
+        return this._photo;
+    }
+    get admin(){
+        return this._admin;
+    }
+    set photo(value){
+        this._photo = value;
+    }
 }
